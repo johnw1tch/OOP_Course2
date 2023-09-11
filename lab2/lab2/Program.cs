629a83b3f9ced980c2a3e41f0dfb8b8e8d146e79
olex3 <olex3105@gmail.com>
2023-09-11 15:53:12 +0300

    new lab task

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Sum
+{
+    private readonly float[] a_var = new float[3];
+    public void SetAvar()
+    {
+        string? temp;
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Enter " + (i + 1) + "th Element");
+            temp = Console.ReadLine();
+            if (temp == null) a_var[i] = 0;
+            else a_var[i] = float.Parse(temp);
+        }
+        return;
+    }
+    public float GetAvar(int n) { return a_var[n]; }
+    public float Execute()
+    {
+        float sum = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            sum += a_var[i] / (i + 1 * (i + 2));
+        }
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        Sum sum = new();
+        sum.SetAvar();
+        Console.WriteLine("The Result is : " + sum.Execute());
+
+    }
+
+
+}
